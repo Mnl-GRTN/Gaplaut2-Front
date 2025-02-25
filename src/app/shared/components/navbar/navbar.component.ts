4a8446d28e8fa51ef9790a723d0afbf10c4676da
GARATTONI Manoël <garattoni.manoel@yahoo.com>
2025-02-25 12:07:29 +0100

    Add Navbar component and integrate into app layout

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+  constructor(private router: Router) { }
+
+  onConnect(): void {
+    this.router.navigate(['/login']);
+  }
+
+}
