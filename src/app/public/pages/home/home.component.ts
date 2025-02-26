a0c5271dfcffd88e27a35ef8d95f22d2e7bc8ab2
GARATTONI Manoël <garattoni.manoel@yahoo.com>
2025-02-26 14:16:14 +0100

    Add AppointmentForm component to home page and implement search/appointment toggle functionality

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { SearchCenterComponent } from '../../components/search-center/search-center.component';
 import { InfoCardComponent } from "../../components/info-card/info-card.component";
+import { AppointmentFormComponent } from '../../components/appointment-form/appointment-form.component';
+import { Center } from '../../../core/services/center';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SearchCenterComponent, InfoCardComponent],
+  imports: [SearchCenterComponent, InfoCardComponent, AppointmentFormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  selectedCentre?: Center;
+
+  onCentreSelected(centre: Center): void {
+    this.selectedCentre = centre;
+    const homepage = document.querySelector('.homepage');
+    homepage?.classList.add('appointment-active');
+
+  }
+
+  onAppointmentSubmitted() {
+    const homepage = document.querySelector('.homepage');
+    homepage?.classList.add('appointment-active');
+  }
+
+  onBackToSearch() {
+    const homepage = document.querySelector('.homepage');
+    homepage?.classList.remove('appointment-active');
+  }
 }
