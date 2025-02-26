f8e3393fcf1eb466df90bf9db0e1a5c18e06e88a
GARATTONI Manoël <garattoni.manoel@yahoo.com>
2025-02-26 14:16:04 +0100

    Refactor Auth component: Enhance login form with Material Design, add password visibility toggle, and improve error handling

--- src/app/public/pages/auth/auth.component.ts
+++ src/app/public/pages/auth/auth.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { NgIf } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [FormsModule, NgIf],
+  imports: [FormsModule, NgIf, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
@@ -15,17 +20,20 @@ export class AuthComponent {
   email: string = '';
   password: string = '';
   errorMessage: string = '';
+  hidePassword: boolean = true; // État pour gérer la visibilité du mot de passe
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  onSubmit(): void {
-    this.authService.login(this.email, this.password).subscribe({
-      next: () => {
-        this.router.navigate(['/test']); // Redirect after login
-      },
-      error: () => {
-        this.errorMessage = "Échec de la connexion. Vérifiez vos identifiants.";
-      }
-    });
+  onSubmit(loginForm: NgForm): void {
+    if (loginForm.valid) {
+      this.authService.login(this.email, this.password).subscribe({
+        next: () => {
+          this.router.navigate(['/test']); // Redirection après connexion réussie
+        },
+        error: () => {
+          this.errorMessage = "Échec de la connexion. Vérifiez vos identifiants.";
+        }
+      });
+    }
   }
 }
