367bd89ace5500cb0d05a6a1ca3c4b0bf536ddd2
GARATTONI Manoël <garattoni.manoel@yahoo.com>
2025-03-01 16:09:28 +0100

    Refactor dashboard layout: implement Material Design components for side menu and content area, enhance styling, and improve responsiveness

--- src/app/private/pages/dashboard/dashboard.component.ts
+++ src/app/private/pages/dashboard/dashboard.component.ts
@@ -3,13 +3,22 @@ import { Router } from '@angular/router';
 import { NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatIcon } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
+
 import { AuthService } from '../../../core/services/auth.service';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NgIf, RouterOutlet],
+  imports: [NgIf, RouterOutlet, MatSidenavModule, MatToolbarModule, MatListModule, MatButtonModule, RouterModule, MatProgressSpinnerModule, MatIcon],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
